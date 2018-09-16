05db37cbcf60c85a721808b6146003a53be30235
Partha Ranjan <partharanjan@live.com>
2018-09-16 11:18:27 +0530

    google map integrated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  map: GoogleMap;
+  constructor(private platform: Platform) { }
+
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: {
+          lat: 43.0741704,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    });
+
+  }
 
 }
